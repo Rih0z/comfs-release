3f25f5c90de0850a50f0bf50114d6f408cf83c48
Rih0z <riho.dare@gmail.com>
2019-02-19 17:24:21 +0900

    please watch client.c

--- client/client.c
+++ client/client.c
@@ -15,7 +15,7 @@ int main(){
     printf("please set up server\n");
     scanf("%s",ctr);
     connect = open("m/TCP4/127.0.0.1/8888/opt",O_RDWR);
-   // connect = open("m/TCP4/localhost/8888/opt",O_RDWR);
+   // connect = open("m/TCP4/localhost/8888/opt",O_RDWR); this is ok too.
     if(connect == -1){
         printf("connect faild\n");
         printf("Plese Setup Server \n");
@@ -24,23 +24,17 @@ int main(){
     }else{
         printf("connect SUCCESS!\n");
     }
-    //存在しないファイルにはアクセスできない
-    //NULLになるのは仕方ない？
     char senddata[256] ;
     int endflag = 1 ;
     while(endflag){
         printf("please input message \n");
         scanf("%s",senddata);
-        //int ret = fwrite(senddata,sizeof(senddata),1,connect);
         int ret = write(connect,senddata,sizeof(senddata));
         if(ret == 0 ){
             printf("No write \n");
         }if(ret < 0 ){
             printf("error");
         }else{
-            //厳密にいうとしっかり書き込まれたかこれではみれない
-            //ブロックサイズまでは書けることが多いが普通のOSの場合512?当たるなでが「
-            //書き込みのサイズを見る
             printf("sizeof %lu data is %s sended\n",sizeof(senddata) ,senddata);
         }
         if(strlen(senddata) == 1)
@@ -50,7 +44,6 @@ int main(){
         printf("if you input any words , reciev a reply\n");
         printf("read start\n");
         char message[256];
-        //  ret = fread(message,256,1,connect);
         ret = read(connect ,message,sizeof(message));
         if(ret < 0 ){
             printf("read faild\n");
